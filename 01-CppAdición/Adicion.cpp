16540735fac464e0f3e23597c14453f1273ae60d
Teo Martin Toledo <teootoledo@gmail.com>
2019-04-21 21:21:22 -0300

    Codeo del programa

---
+++ 01-CppAdición/Adicion.cpp
@@ -0,0 +1,15 @@
+/*  Adición
+    Toledo, Teo Martin
+    21/04/2019
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    double x1;
+    cin >> x1;
+    double x2;
+    cin >> x2;
+    cout << (x1 + x2);
+    return 0;
+}
